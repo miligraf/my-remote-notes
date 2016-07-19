066a5b8d9255723508d82bea3f688a321d5b9888
Milton Andrade <miligraf@gmail.com>
2016-07-19 16:48:27 -0400

    add form to edite note

--- www/js/app.js
+++ www/js/app.js
@@ -1,41 +1,76 @@
-var app = angular.module('starter', ['ionic']);
+(function() {
 
-app.controller('ListCtrl', function($scope) {
-  $scope.notes = [
-    {
-      title: 'First Note', 
-      description: 'First description'
-    },
-    {
-      title: 'Second Note', 
-      description: 'Second description'
-    }
-  ];
-});
+  var app = angular.module('starter', ['ionic']);
 
-app.config(function($stateProvider, $urlRouterProvider){
-  $stateProvider.state('list', {
-    url: '/list',
-    templateUrl: 'templates/list.html'
+  app.config(function($stateProvider, $urlRouterProvider){
+    $stateProvider.state('list', {
+      url: '/list',
+      templateUrl: 'templates/list.html'
+    });
+
+    $stateProvider.state('edit', {
+      url: '/edit/:noteId',
+      templateUrl: 'templates/edit.html'
+    });
+
+    $urlRouterProvider.otherwise('/list');
   });
 
-  $stateProvider.state('edit', {
-    url: '/edit',
-    templateUrl: 'templates/edit.html'
+  var notes = [
+      {
+        id: '1',
+        title: 'First Note', 
+        description: 'First description'
+      },
+      {
+        id: '2',
+        title: 'Second Note', 
+        description: 'Second description'
+      }
+    ];
+
+  function getNote(noteId) {
+    for (var i = 0; i < notes.length; i++) {
+      if (notes[i].id === noteId) {
+        return notes[i];
+      }
+    }
+    return undefined;
+  }
+
+  function updateNote(note) {
+    for (var i = 0; i < notes.length; i++) {
+      if (notes[i].id === note.id) {
+        notes[i] = note;
+        return;
+      }
+    }
+  }
+
+  app.controller('ListCtrl', function($scope) {
+    $scope.notes = notes;
   });
 
-  $urlRouterProvider.otherwise('/list');
-});
+  app.controller('EditCtrl', function($scope, $state) {
+    $scope.note = angular.copy(getNote($state.params.noteId));
 
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+    $scope.save = function() {
+      updateNote($scope.note);
+      $state.go('list');
+    };
   });
-})
+
+  app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  });
+
+}());

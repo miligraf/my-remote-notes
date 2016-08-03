404eb85630d46d61cd30c03994781658d6e85dab
Milton Andrade <miligraf@gmail.com>
2016-08-02 21:08:28 -0400

    use localstorage for notes

--- www/js/notestore.js
+++ www/js/notestore.js
@@ -1,6 +1,11 @@
 angular.module('mynotes.notestore', [])
   .factory('NoteStore', function() {
-    var notes = [];
+
+    var notes = angular.fromJson(window.localStorage['notes'] || '[]');
+
+    function persist() {
+      window.localStorage['notes'] = angular.toJson(notes);
+    }
 
     return {
       list: function() {
@@ -18,12 +23,14 @@ angular.module('mynotes.notestore', [])
 
       create: function(note) {
         notes.push(note);
+        persist();
       },
 
       update: function(note) {
         for (var i = 0; i < notes.length; i++) {
           if (notes[i].id === note.id) {
             notes[i] = note;
+            persist();
             return;
           }
         }
